0ec1bc081096358aab7e4873360aa92c8bbaa29f
luciatesta <luciatesta135@gmail.com>
2026-04-19 18:17:38 -0300

    correccion tp-4

--- TP-4/index.js
+++ TP-4/index.js
@@ -4,23 +4,35 @@ import { ProductManager } from "./TP-1/ProductManager";
 const app = express();
 const PORT = 8080;
 
+app.use(express.js());
+app.use(express.urlencoded({ extended: true }))
+
 const productManager = new ProductManager('./TP-1/productos.json');
 
 app.get('api/products', async (req, res) => {
-    try{
+    try {
         let data = await productManager.getProducts();
         console.log(data);
+        res.json(data);
 
-    }catch('error', (err)){
-        console.error('Error: ' + err.menssage)
+    } catch (err) {
+        console.error('Error: ' + err.menssage);
+        res.status(500).json({ error: 'Error al obtener los productos' })
     }
 })
 
-app.get('api/products/id', async (req, res) => {
-    try{
+app.get('api/products/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
         let data = await productManager.getProductoById();
         console.log(data);
-    }catch('error, (err)'){
-        console.error('Error: ' + err.menssage)
+        if (!data) {
+            return res.status(404).json({ error: 'Producto no encontrado' });
+        }
+        res.json(data);
+
+    } catch (err) {
+        console.error('Error: ' + err.menssage);
+        res.status(500).json({ error: 'Error al obtener el producto' });
     }
-})
+});
